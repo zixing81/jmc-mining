8e57588b005e7d406287da3f05e0a068ba8ff072
zixing81 <44152915+zixing81@users.noreply.github.com>
2018-10-15 16:16:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
     engines = require('consolidate'),
     assert = require('assert'),
     ObjectId = require('mongodb').ObjectID,
-    url = 'mongodb://c4ts-jmc-mining-dgll:98RKyJBIWmBZwzDSAvKLVXmkmIvc7iEeBZHt1GlDIKKMItlIP4hki2XmPIr7Odno6w7a6JrilKB1uPJlx5MULg==@c4ts-jmc-mining-dgll.documents.azure.com:10255/?ssl=true&replicaSet=globaldbe';
+    url = 'mongodb://c4ts-jmc-mining-dgll:98RKyJBIWmBZwzDSAvKLVXmkmIvc7iEeBZHt1GlDIKKMItlIP4hki2XmPIr7Odno6w7a6JrilKB1uPJlx5MULg==@c4ts-jmc-mining-dgll.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
 
 app.use(express.static(__dirname + "/public"));
 
